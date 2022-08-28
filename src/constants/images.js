92d5584beebdb403cb6dbe029f63ff59f8962db6
Bogdan <bogdan_kolesov@bk.ru>
2022-08-28 14:48:25 +0600

    fix

--- src/constants/images.js
+++ src/constants/images.js
@@ -16,7 +16,37 @@ import cumuloNimbus from '../assets/images/cloudAtlas/cumuloNimbus.jpg'
 import dove from '../assets/images/directoryData/dove.jpg'
 import codes from '../assets/images/directoryData/codes.jpg'
 
+//Облака верхнего яруса
 import cirrusFibratus from '../assets/images/cloudAtlas/height/cirrusFibratus.jpg'
+import cirroCumulusUndulatus from '../assets/images/cloudAtlas/height/cirroCumulusUndulatus.jpg'
+import cirroStratusFibratus from '../assets/images/cloudAtlas/height/cirroStratusFibratus.jpg'
+import cirrusIntortus from '../assets/images/cloudAtlas/height/cirrusIntortus.jpg'
+import cirrusIntortusNebulosus from '../assets/images/cloudAtlas/height/cirrusIntortusNebulosus.jpg'
+import cirrusSpissatus from '../assets/images/cloudAtlas/height/cirrusSpissatus.jpg'
+import cirrusTraktus from '../assets/images/cloudAtlas/height/cirrusTraktus.jpg'
+import cirrusUncinus from '../assets/images/cloudAtlas/height/cirrusUncinus.jpg'
+import cirrusVertebratus from '../assets/images/cloudAtlas/height/cirrusVertebratus.jpg'
+import cirroCumulusCumuliformis from '../assets/images/cloudAtlas/height/cirroCumulusCumuliformis.jpg'
+//Облака среднего яруса
+import altoCumulusFloccus from '../assets/images/cloudAtlas/middle/altoCumulusFloccus.jpg'
+import altoCumulusLenticularis from '../assets/images/cloudAtlas/middle/altoCumulusLenticularis.jpg'
+import altoCumulusTranslucidus from '../assets/images/cloudAtlas/middle/altoCumulusTranslucidus.jpg'
+import altoStratusOpacus from '../assets/images/cloudAtlas/middle/altoStratusOpacus.jpg'
+import altoStratusTranslucidus from '../assets/images/cloudAtlas/middle/altoStratusTranslucidus.jpg'
+import cumulusHumilis from '../assets/images/cloudAtlas/middle/cumulusHumilis.jpg'
+//Облака нижнего яруса
+import fractoNimbus from '../assets/images/cloudAtlas/low/fractoNimbus.jpg'
+import orographClouds from '../assets/images/cloudAtlas/low/orographClouds.jpg'
+import stratoCumulusDiurnalis from '../assets/images/cloudAtlas/low/stratoCumulusDiurnalis.jpg'
+import stratoCumulusOpacus from '../assets/images/cloudAtlas/low/stratoCumulusOpacus.jpg'
+import stratoCumulusTranslucidus from '../assets/images/cloudAtlas/low/stratoCumulusTranslucidus.jpg'
+import stratuCumulusVesperalis from '../assets/images/cloudAtlas/low/stratuCumulusVesperalis.jpg'
+import stratusNebulosus from '../assets/images/cloudAtlas/low/stratusNebulosus.jpg'
+import stratusUndulatus from '../assets/images/cloudAtlas/low/stratusUndulatus.jpg'
+//облака вертикального развития
+import cumuloNimbusAtlas from '../assets/images/cloudAtlas/vertical/cumuloNimbus.jpg'
+import cumulusCongestus from '../assets/images/cloudAtlas/vertical/cumulusCongestus.jpg'
+import cumulusMediocris from '../assets/images/cloudAtlas/vertical/cumulusMediocris.jpg'
 
 
 export {
@@ -37,5 +67,31 @@ export {
     cumuloNimbus,
     dove,
     codes,
-    cirrusFibratus
+    cirrusFibratus,
+    cirroCumulusUndulatus,
+    cirroStratusFibratus,
+    cirrusIntortus,
+    cirrusIntortusNebulosus,
+    cirrusSpissatus,
+    cirrusTraktus,
+    cirrusUncinus,
+    cirrusVertebratus,
+    cirroCumulusCumuliformis,
+    altoCumulusFloccus,
+    altoCumulusLenticularis,
+    altoCumulusTranslucidus,
+    altoStratusOpacus,
+    altoStratusTranslucidus,
+    cumulusHumilis,
+    fractoNimbus,
+    orographClouds,
+    stratoCumulusDiurnalis,
+    stratoCumulusOpacus,
+    stratoCumulusTranslucidus,
+    stratuCumulusVesperalis,
+    stratusNebulosus,
+    stratusUndulatus,
+    cumuloNimbusAtlas,
+    cumulusCongestus,
+    cumulusMediocris,
 }
